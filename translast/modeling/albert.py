e730abb3eaed68ec0eba9ca82f1beab9aba6e840
Andres D. Chamorro-Parejo <andresdavidchamorro@gmail.com>
2025-03-07 14:26:11 -0600

    albert token type (segment) embeding

--- translast/modeling/albert.py
+++ translast/modeling/albert.py
@@ -55,20 +55,26 @@ class AlbertEmbeddings(nn.Module):
         self.word_embeddings = nn.Embedding(config.vocab_size, config.embedding_size)
         self.embedding_to_hidden = nn.Linear(config.embedding_size, config.hidden_size)
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.segment_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=1e-12)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-    def forward(self, input_ids, position_ids=None):
+    def forward(self, input_ids, token_type_ids=None, position_ids=None):
         input_shape = input_ids.size()
         seq_length = input_shape[1]
         if position_ids is None:
             position_ids = torch.arange(seq_length, dtype=torch.long, device=input_ids.device)
             position_ids = position_ids.unsqueeze(0).expand(input_shape) # (S,) -> (B, S)
 
+        if token_type_ids is None:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=input_ids.device)
+        
         words_embeddings = self.word_embeddings(input_ids)
         words_embeddings = self.embedding_to_hidden(words_embeddings)
         position_embeddings = self.position_embeddings(position_ids)
-        embeddings = words_embeddings + position_embeddings
+        segment_embeddings = self.segment_embeddings(token_type_ids)
+
+        embeddings = words_embeddings + position_embeddings + segment_embeddings
         embeddings = self.LayerNorm(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
