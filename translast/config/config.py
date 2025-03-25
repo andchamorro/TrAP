6848e1b1fcff15d3b509f7d4c323d60079fa4966
Andres D. Chamorro-Parejo <andresdavidchamorro@gmail.com>
2025-03-25 15:53:19 -0500

    Accelerator fsdp config setup

--- translast/config/config.py
+++ translast/config/config.py
@@ -17,7 +17,7 @@ PROCESSED_DATA_DIR = DATA_DIR / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
 MODELS_DIR = PROJ_ROOT / "models"
-MODELS_CONFIG_DIR = MODELS_DIR / "config"
+CONFIG_DIR = PROJ_ROOT / "config"
 
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
--- translast/modeling/train.py
+++ translast/modeling/train.py
@@ -1,4 +1,5 @@
 import os
+import io
 import typer
 import json
 from loguru import logger
@@ -15,7 +16,7 @@ from datasets import load_from_disk
 
 # from translast.modeling.albert import AlbertConfig, AlbertForMaskedLM, AlbertModel
 from translast.loaders.tokenizer import WholeKmerMaskingDataCollator
-from translast.config.config import MODELS_CONFIG_DIR, MODELS_DIR, PROCESSED_DATA_DIR
+from translast.config.config import CONFIG_DIR, MODELS_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
@@ -100,9 +101,9 @@ def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
     model_name: str = typer.Argument(help="Name of the model will be saved"),
     pretrained_tokenizer_path: Path = typer.Option(default=..., help="Path to the pretrained tokenizer"),
-    trainer_config_path: Path = typer.Option(MODELS_CONFIG_DIR / "trainer_config.json", help="Path to the trainer config"),
-    albert_config_path: Path = typer.Option(MODELS_CONFIG_DIR / "albert_config.json", help="Path to the Albert config"),
-    fsdp_config: Path = typer.Option(None, help="Config to be used with fsdp (Pytorch Distributed Parallel Training)."),
+    trainer_config_path: Path = typer.Option(CONFIG_DIR / "trainer_config_fsdp.json", help="Path to the trainer config"),
+    albert_config_path: Path = typer.Option(CONFIG_DIR / "albert_config.json", help="Path to the Albert config"),
+    fsdp_config: str = typer.Option(None, help="Config to be used with fsdp (Pytorch Distributed Parallel Training)."),
     builder: str = typer.Option(None, help="Path to the genome dataset"),
     k: int = typer.Option(18, help="K-mer size"),
     test_split: float = typer.Option(0.1, help="Test split ratio"),
@@ -114,10 +115,13 @@ def main(
 ):
     debug_callback(debug)
     logger.info("Training ALBERT model...")
-    trainer_args = TrainingArguments(**json.load(open(trainer_config_path, "r")))
-    if fsdp_config is not None:
-        trainer_args.fsdp_config = fsdp_config
-        pass
+    with open(trainer_config_path, 'r') as trainer_config_file:
+        trainer_config = json.load(trainer_config_file)
+        if fsdp_config is not None:
+            trainer_config["fsdp_config"] = fsdp_config
+            pass
+
+    trainer_args = TrainingArguments(**trainer_config)
     trainer_args.output_dir = os.path.join(MODELS_DIR, model_name)
     trainer_args.dataloader_num_workers = num_workers
     model = AlbertForMaskedLM(AlbertConfig.from_json_file(albert_config_path))
